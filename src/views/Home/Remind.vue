f49631f352e412eadcbae6d04122ebdfc4494ce6
吴宇迪 <2487136046@qq.com>
2020-11-12 11:33:57 +0800

    滚动更新

--- src/views/Home/Remind.vue
+++ src/views/Home/Remind.vue
@@ -16,34 +16,48 @@
     <van-list
       v-model="loading"
       :finished="finished"
+      :offset="offset"
       finished-text="没有更多了"
       @load="onLoad"
     >
-    <div
-      v-for="(item, index) in recent_contact"
-      :key="index"
-      class="latest_tasks"
-    >
-      <ul>
-        <li style="font-weight: 550">{{ item.typeName }}</li>
-        <li class="set_up" @click="updateStatus(item)">
-          {{
-            item.status == 1 ? "设为已读" : item.status == 2 ? "设为未读" : ""
-          }}
-        </li>
-        <li>{{ item.createdTime | transform }}</li>
-      </ul>
-      <ul>
-        <li>{{ item.content }}</li>
-      </ul>
-    </div>
+      <div ref="box">
+        <div
+          v-for="(item, index) in recent_contact"
+          :key="index"
+          class="latest_tasks"
+        >
+          <ul>
+            <li style="font-weight: 550">{{ item.typeName }}</li>
+            <li class="set_up" @click="updateStatus(item)">
+              {{
+                item.status == 1
+                  ? "设为已读"
+                  : item.status == 2
+                  ? "设为未读"
+                  : ""
+              }}
+            </li>
+            <li>{{ item.createdTime | transform }}</li>
+          </ul>
+          <ul>
+            <li>{{ item.content }}</li>
+          </ul>
+        </div>
+      </div>
     </van-list>
     <!-- <van-divider :style="{ borderColor: '#fff' }">已加载完毕</van-divider> -->
   </div>
 </template>
-<script>
+<script >
 import ChildNav from "../../components/Public/ChildNav";
-import { Dialog, Toast } from "vant";
+import { Dialog, Toast, List } from "vant";
+import "vant/lib/index.css";
+// import BScroll from '@better-scroll/core'
+// console.log(BScroll);
+// function BScroll() {
+//   debugger
+// }
+
 export default {
   name: "Remind",
   components: {
@@ -57,31 +71,67 @@ export default {
       value2: "0",
       option1: [],
       option2: [],
-      loading: false,
-      finished: false,
-      throttleTime: {
-        lastTime: 0,
-        nowTime: 0
-      }
+      offset: 0, //滚动条与底部距离小于 offset 时触发load事件，默认300
+      pageNo: 1, // 当前页码
+      pageSize: 10, // 分页大小
+      total: 0, // 查询总条数
+      loading: false, // 滚动加载中
+      finished: false, // 滚动加载完成
+      charityData: [],
     };
   },
   created() {
     this.dic_nation();
-    // this.queryTask();
   },
   methods: {
     queryTask() {
-      let _username = localStorage.getItem("username");
-      return this.$httpGet({
-        url: "/api/userMessage/query",
-        params: {
-          userName: _username,
-          limit: 10,
-          page: 1,
-        },
-      }).then((res) => {
-        this.recent_contact = res.data;
-      })
+      return new Promise((resolve, reject) => {
+        let params = {
+          userName: localStorage.getItem("username"),
+          page: this.pageNo,
+          limit: this.pageSize,
+        };
+        this.$httpGet({
+          url: "/api/userMessage/query",
+          params: params,
+        })
+          .then((res) => {
+            console.log(res);
+            if (res.data.length > 0) {
+              console.log(1111);
+              let result = {
+                total: res.count,
+                pageIndex: 1,
+                recent_contact: res.data,
+              };
+              resolve(result);
+            }
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    // 滚动加载更多
+    onLoad() {
+      this.loading = true;
+      this.queryTask().then((res) => {
+        console.log(res);
+        this.recent_contact = this.recent_contact.concat(res.recent_contact);
+        if (this.recent_contact.length >= res.total) {
+          this.finished = true;
+        } else {
+          this.finished = false;
+          this.pageNo = this.pageNo + 1;
+        }
+        this.loading = false;
+      });
+    },
+    alert(msg) {
+      this.$toast.showToast({
+        msg: msg,
+        duration: 2000,
+      });
     },
     dic_nation() {
       this.$httpGet({
@@ -137,50 +187,28 @@ export default {
     },
     updateStatus(item) {
       this.throttleTime.nowTime = new Date().getTime();
-      if(this.throttleTime.nowTime - this.throttleTime.lastTime > 2000) {
-           this.throttleTime.lastTime = this.throttleTime.nowTime;
-           this.$httpPut({
-            url: "/api/userMessage/updateMessageStatus",
-            params: {
-              messageId: item.id,
-            },
-          }).then((res) => {
-            if (res.code == 400) {
-              Toast({
-                message: res.resultMessage,
-                position: "middle",
-              });
-            } else {
-              this.queryTask();
-              Toast({
-                message: res.resultMessage,
-                position: "middle",
-              });
-            }
-          });
+      if (this.throttleTime.nowTime - this.throttleTime.lastTime > 2000) {
+        this.throttleTime.lastTime = this.throttleTime.nowTime;
+        this.$httpPut({
+          url: "/api/userMessage/updateMessageStatus",
+          params: {
+            messageId: item.id,
+          },
+        }).then((res) => {
+          if (res.code == 400) {
+            Toast({
+              message: res.resultMessage,
+              position: "middle",
+            });
+          } else {
+            this.queryTask();
+            Toast({
+              message: res.resultMessage,
+              position: "middle",
+            });
+          }
+        });
       }
-     
-    },
-    onLoad() {
-      console.log(123);
-      this.queryTask().then(() => {
-        this.finished = true
-      })
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      // setTimeout(() => {
-      //   for (let i = 0; i < 10; i++) {
-      //     this.recent_contact.push(this.recent_contact.length + 1);
-      //   }
-
-      //   // 加载状态结束
-      //   this.loading = false;
-
-      //   // 数据全部加载完成
-      //   if (this.recent_contact.length >= 40) {
-      //     this.finished = true;
-      //   }
-      // }, 1000);
     },
   },
 };
